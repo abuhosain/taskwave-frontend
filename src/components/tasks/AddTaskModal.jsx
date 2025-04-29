0e56e473934af105c2f04f010241471811bbe2af
Hosain <cryptichasan925@gmail.com>
2025-04-29 19:16:32 +0600

    re usabale modal done

---
+++ src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,13 @@
+import Modal from "../ui/Modal"
+
+ 
+
+const AddTaskModal = ({isOpen, setIsOpen}) => {
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={"Abu Hosain"}>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta atque error dolor architecto veritatis molestiae autem corrupti, cupiditate nulla dolore fuga laudantium culpa, modi adipisci maiores. Modi, eos tenetur? Necessitatibus.
+  </Modal>
+  )
+}
+
+export default AddTaskModal
