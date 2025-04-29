445b5ef40012037db2aa17dd70aca84ef31924c6
Hosain <cryptichasan925@gmail.com>
2025-04-29 20:04:56 +0600

    task card done

--- src/redux/features/tasks/taskSlice.js
+++ src/redux/features/tasks/taskSlice.js
@@ -1,7 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  tasks: [],
+  tasks: [
+    {
+      id: 1,
+      status: 'pending',
+      title: 'Remove Button',
+      description:
+        'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
+      date: '2023-08-28',
+      assignedTo: 'Mir Hussain',
+      priority: 'high',
+    },
+    {
+      id: 2,
+      status: 'pending',
+      title: 'Remove Button',
+      description:
+        'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
+      date: '2023-08-28',
+      assignedTo: 'Mir Hussain',
+      priority: 'high',
+    },
+    {
+      id: 3,
+      status: 'pending',
+      title: 'Remove Button',
+      description:
+        'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
+      date: '2023-08-28',
+      assignedTo: 'Mir Hussain',
+      priority: 'high',
+    }
+  ],
 };
 const tasksSlice = createSlice({
   name: "tasks",
@@ -9,13 +40,17 @@ const tasksSlice = createSlice({
   reducers: {
     addTask: (state, { payload }) => {
       if (state.tasks.length === 0) {
-        state.tasks.push({ id: 1, ...payload });
+        state.tasks.push({ id: 1, status: "pending", ...payload });
       } else {
         const lastTask = state.tasks.at(-1);
         const newId = lastTask.id + 1;
-        state.tasks.push({ id: newId, ...payload });
+        state.tasks.push({ id: newId, status: "pending", ...payload });
       }
     },
+
+    removeTask: (state, {payload}) => {
+      state.tasks = state.tasks.filter((task) => task.id !== payload);
+    }
   },
 });
 
