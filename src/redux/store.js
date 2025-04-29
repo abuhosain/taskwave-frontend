a354cf1daa8d76c5684202ca7b5d3461bf308c89
Hosain <cryptichasan925@gmail.com>
2025-04-29 11:39:50 +0600

    task for redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import tasksSlice from "./features/tasks/taskSlice"
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    tasks: tasksSlice
+  },
 });
 
 export default store;
